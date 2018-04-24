35ec746b53dedee942532650a737bbaf9936c40a
lanyanguang32 <lanyanguang32@gmail.com>
2018-04-24 14:40:47 +0800

    modify 3d api

--- app/Http/Controllers/Api/v1/WarpController.php
+++ app/Http/Controllers/Api/v1/WarpController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Api\v1\ApiController;
 use App\Modcat;
 use App\Mod;
+use App\Sku;
 use App\Transformers\ModcatTransformer;
 use App\Transformers\ModTransformer;
 use App\Transformers\WarpTransformer;
@@ -39,9 +40,38 @@ class WarpController extends ApiController
       //move_x=0
       //move_y=0
       //zoom=100
+      $mod = Mod::find($request->mod_id);
+      $goods = Sku::find($request->goods_id);
 
-      return $this->response->array([
-      	'image'=> 'http://47.104.186.103:80/img/fitting/out/12.jpg',
-      ]);
+      $model = $mod->name;
+      $clothPath = 'upload/' . $goods->sku.'.jpg';
+      $move_x = $request->input('move_x', 0); 
+      $move_y = $request->input('move_y', 0); 
+      $zoom = $request->input('zoom', 100); 
+
+      $url = "http://47.104.186.103/fitting/api/ef.jsp";
+      $param =  [
+        'model'=>$model,
+        'clothPath'=>$clothPath,
+        'move_x'=>$move_x,
+        'move_y'=>$move_y,
+        'zoom'=>$zoom
+      ];
+
+
+      $return = file_get_contents($url . '?' .http_build_query($param));
+      
+      $return = json_decode($return);
+
+
+      if($return->type == 'true'){
+        return $this->response->array([
+                    'image'=> $return->imgName,
+              ]);
+      }else{
+        return $this->response->array([
+            'image'=> 'http://47.104.186.103/img//fitting/out/default/ef.jpg'
+         ]);
+      }
     }
 }
